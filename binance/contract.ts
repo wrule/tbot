04aea07b0ba52c3856a4d06c2925d6b22d4284d3
jimao <jimao@jimaodeMBP.lan>
2022-06-13 23:23:31 +0800

    Open和Close

--- binance/contract.ts
+++ binance/contract.ts
@@ -1,4 +1,5 @@
 import { binance } from 'ccxt';
+import { Direction, Side } from '../struct/common';
 
 export
 class Contract {
@@ -6,11 +7,33 @@ class Contract {
     private readonly client: binance,
   ) { }
 
-  public async Open(symbol: string) {
-
+  public async Open(
+    symbol: string,
+    direction: Direction,
+    amount: number,
+  ) {
+    return await this.client.createOrder(
+      symbol,
+      'MARKET',
+      { 'LONG': 'buy', 'SHORT': 'sell', }[direction] as Side,
+      amount,
+      undefined,
+      { positionSide: direction, },
+    );
   }
 
-  public async Close() {
-
+  public async Close(
+    symbol: string,
+    direction: Direction,
+    amount: number,
+  ) {
+    return await this.client.createOrder(
+      symbol,
+      'MARKET',
+      { 'LONG': 'sell', 'SHORT': 'buy', }[direction] as Side,
+      amount,
+      undefined,
+      { positionSide: direction, },
+    );
   }
 }
