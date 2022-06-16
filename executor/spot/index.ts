e52bac2bca30d5e63963cc75bcb4ac792c593818
jimao <jimao@jimaodeMacBook-Pro.local>
2022-06-16 17:54:29 +0800

    提交

---
+++ executor/spot/index.ts
@@ -0,0 +1,26 @@
+import { ITransaction } from './transaction';
+
+export
+interface ISpotExecutor {
+  Buy(
+    in_asset: number,
+    price: number,
+    time: number,
+  ): ITransaction | null;
+
+  BuyAll(
+    price: number,
+    time: number,
+  ): ITransaction | null;
+
+  Sell(
+    in_asset: number,
+    price: number,
+    time: number,
+  ): ITransaction | null;
+
+  SellAll(
+    price: number,
+    time: number,
+  ): ITransaction | null;
+}
