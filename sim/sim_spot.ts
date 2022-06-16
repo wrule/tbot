2390582dc46c14a94d8a0df95d4886d8aff69595
jimao <jimao@jimaodeMacBook-Pro.local>
2022-06-16 16:09:25 +0800

    提交

---
+++ sim/sim_spot.ts
@@ -0,0 +1,19 @@
+
+export
+class SimSpot {
+  public constructor(
+    private funds = 100,
+  ) {
+
+  }
+
+  private assets = 0;
+
+  public Buy(amount: number, price: number) {
+
+  }
+
+  public Sell() {
+
+  }
+}
