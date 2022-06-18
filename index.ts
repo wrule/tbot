ff4c736edd7a1e8954a289f14199bb095cd5cf30
jimao <jimao@jimaodeMBP.lan>
2022-06-18 19:16:56 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import { binance } from 'ccxt';
+// import tulind from 'tulind';
+const tulind = require('tulind');
 import { BinanceSpot } from './executor/spot/binance_spot';
 import secret from './.secret.json';
 import { TwoLineCross } from './robot/spot/two_line_cross';
@@ -17,9 +19,24 @@ async function main() {
   const robot = new TwoLineCross(executor);
 
   setInterval(async () => {
-    const list = await client.fetchOHLCV('LINK/USDT', '1m', undefined, 500);
-    const a = ArrayToKLine(list);
-    console.log(a[a.length - 1].time, a[a.length - 1].close);
+    const list = await client.fetchOHLCV('LINK/USDT', '1m', undefined, 10);
+    const kline = ArrayToKLine(list).filter((item) => item.confirmed);
+    const closes = kline.map((item) => item.close);
+    tulind.indicators.sma.indicator(
+      [closes],
+      [3],
+      (err: any, result: any) => {
+        console.log(3, result[0]);
+      },
+    );
+    tulind.indicators.sma.indicator(
+      [closes],
+      [8],
+      (err: any, result: any) => {
+        console.log(8, result[0]);
+      },
+    );
+    console.log(Number(new Date()));
   }, 1000);
 }
 
