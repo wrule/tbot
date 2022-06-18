ae707f5cbbd75348bfe87076cfd5f2086a3296f9
jimao <jimao@jimaodeMBP.lan>
2022-06-18 12:36:20 +0800

    注释

--- executor/spot/transaction.ts
+++ executor/spot/transaction.ts
@@ -1,14 +1,44 @@
 
 export
 interface ITransaction {
+  /**
+   * 请求时间
+   */
   request_time: number;
+  /**
+   * 成交时间
+   */
   transaction_time: number;
+  /**
+   * 响应时间
+   */
   response_time: number;
+  /**
+   * 期望价格
+   */
   expected_price: number;
+  /**
+   * 成交价格
+   */
   price: number;
+  /**
+   * 输入资产名称
+   */
   in_name: string;
+  /**
+   * 期望输入资产数量
+   */
   expected_in_amount: number;
+  /**
+   * 成交输入资产数量
+   */
   in_amount: number;
+  /**
+   * 输出资产名称
+   */
   out_name: string;
+  /**
+   * 输出资产数量
+   */
   out_amount: number;
 }
