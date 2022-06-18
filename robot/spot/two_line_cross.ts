d23f530d54cc0d602f8eb5699c74d38ea865aaca
jimao <jimao@jimaodeMBP.lan>
2022-06-18 17:45:10 +0800

    死叉金叉检测

--- robot/spot/two_line_cross.ts
+++ robot/spot/two_line_cross.ts
@@ -1,4 +1,5 @@
 import { SpotRobot } from '.';
+import { IOHLCV } from '../../struct/ohlcv';
 
 export
 class TwoLineCross
@@ -8,18 +9,19 @@ extends SpotRobot {
     prev_slow: number,
     last_fast: number,
     last_slow: number,
+    ohlcv: IOHLCV,
   ) {
     if ( // 金叉检测
       (prev_fast <= prev_slow) &&
       (last_fast > last_slow)
     ) {
-      this.executor.BuyAll();
+      this.executor.BuyAll(ohlcv.close, ohlcv.time);
     }
     else if ( // 死叉检测
       (prev_fast >= prev_slow) &&
       (last_fast < last_slow)
     ) {
-      this.executor.SellAll();
+      this.executor.SellAll(ohlcv.close, ohlcv.time);
     }
   }
 }
