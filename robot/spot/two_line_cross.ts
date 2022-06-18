85c56fdd32ab473425d8d4fc9301f239b13a43d6
jimao <jimao@jimaodeMBP.lan>
2022-06-18 18:01:58 +0800

    双线穿越机器人完成

--- robot/spot/two_line_cross.ts
+++ robot/spot/two_line_cross.ts
@@ -4,6 +4,8 @@ import { IOHLCV } from '../../struct/ohlcv';
 export
 class TwoLineCross
 extends SpotRobot {
+  private check_time = -1;
+
   public Check(
     prev_fast: number,
     prev_slow: number,
@@ -15,13 +17,19 @@ extends SpotRobot {
       (prev_fast <= prev_slow) &&
       (last_fast > last_slow)
     ) {
-      this.executor.BuyAll(ohlcv.close, ohlcv.time);
+      if (this.check_time !== ohlcv.time) {
+        this.check_time = ohlcv.time;
+        this.executor.BuyAll(ohlcv.close, ohlcv.time);
+      }
     }
     else if ( // 死叉检测
       (prev_fast >= prev_slow) &&
       (last_fast < last_slow)
     ) {
-      this.executor.SellAll(ohlcv.close, ohlcv.time);
+      if (this.check_time !== ohlcv.time) {
+        this.check_time = ohlcv.time;
+        this.executor.SellAll(ohlcv.close, ohlcv.time);
+      }
     }
   }
 }
