d1776195ef3b33fb2791e593f3d316208ed93587
jimao <jimao@jimaodeMBP.lan>
2022-06-19 16:58:08 +0800

    提交代码

--- executor/contract/index.ts
+++ executor/contract/index.ts
@@ -1,10 +1,31 @@
-
+import { Side } from '../../struct/common';
+import { ReturnTransactionAS } from '../spot';
 
 export
 interface IContractExecutor {
   Open(
+    side: Side,
     in_asset: number,
     price?: number,
     time?: number,
-  ):
+  ): ReturnTransactionAS;
+
+  OpenAll(
+    side: Side,
+    price?: number,
+    time?: number,
+  ): ReturnTransactionAS;
+
+  Close(
+    side: Side,
+    in_asset: number,
+    price?: number,
+    time?: number,
+  ): ReturnTransactionAS;
+
+  CloseAll(
+    side: Side,
+    price?: number,
+    time?: number,
+  ): ReturnTransactionAS;
 }
