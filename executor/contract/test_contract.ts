f1b5ac0b6283b416b9571b02f6b82d03db474968
jimao <jimao@jimaodeMBP.lan>
2022-06-19 20:51:17 +0800

    提交代码

--- executor/contract/test_contract.ts
+++ executor/contract/test_contract.ts
@@ -1,4 +1,4 @@
-import { Side } from '../../struct/common';
+import { Direction, Side } from '../../struct/common';
 import { ITransaction } from '../../struct/transaction';
 
 export
@@ -15,30 +15,47 @@ class TestContract {
   }
 
   private deposit!: number;
+  private funds!: number;
   private fee_multiplier!: number;
   private transactions!: ITransaction[];
 
-  private get funds() {
-    return this.deposit * this.lever;
-  }
 
-
+  private assets = {
+    'LONG': 0,
+    'SHORT': 0,
+  };
 
   public Open(
-    side: Side,
-    in_asset: number,
-    price?: number,
-    time?: number,
+    direction: Direction,
+    in_assets: number,
+    price: number,
+    time: number,
   ) {
-    const fee = in_asset * this.fee;
-    if (in_asset <= this.funds && this.deposit > fee) {
-
+    if (in_assets <= this.funds) {
+      this.funds -= in_assets;
+      const out_assets = in_assets / price;
+      this.assets[direction] += out_assets;
     }
+    return null;
+  }
+
+  public Close(
+    direction: Direction,
+    in_assets: number,
+    price: number,
+    time: number,
+  ) {
+    // const fee = in_assets * this.fee;
+    // if (in_assets <= this.funds && this.deposit > fee) {
+    //   const out_assets = in_assets / price;
+    //   this.assets[direction] += out_assets;
+    // }
     return null;
   }
 
   public Reset() {
     this.deposit = this.init_deposit;
+    this.funds = this.deposit * this.lever;
     this.fee_multiplier = 1 - this.fee;
     this.transactions = [];
   }
